cc4de91ad87c91d8879671d22fb91435538aee46
Akshaya <61582763+AkshayaKulasekaran@users.noreply.github.com>
2020-09-04 15:05:10 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,8 +2,11 @@ def calc(first_num, choice, second_num):
     if(choice=='addition'):
         return first_num+second_num
     elif (choice=='subtraction'):
-        return first_num-
-    second_num
+        return first_num-second_num
+    elif(choice=='multiplication'):
+        return first_num*second_num
+    elif (choice=='division'):
+        return first_num/econd_num
 
 print("This is the Basic Calculator Program")
 start=input("Do you wish to start? Type 1 for YES and 0 for NO: ")
@@ -13,7 +16,7 @@ if(st!=1 and st!=0):
 while (st==1):
     num1=input("Enter the first number: ")
     first_num=int(num1)
-    choice=input("Addition or Subtraction? ")
+    choice=input("Addition, subtraction, multiplication or division? ")
     choice=choice.lower()
     num2=input("Enter the second number: ")
     second_num=int(num2)
