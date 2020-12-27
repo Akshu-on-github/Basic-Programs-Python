9856aba115f2068fdc478a57df41851b8dadf857
Akshu <61582763+Akshu-on-github@users.noreply.github.com>
2020-12-27 16:59:21 +0530

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -1,17 +1,17 @@
-num = int(input("Enter a number: "))
+# Armstrong number: A three-digit number whose sum of the cubes of its digits is equal to the number itself
+# Function to check if the given number is an Armstrong number
+def checkArmstrong(num):
+   sum = 0
+   temp = num
+   while temp > 0:
+      digit = temp % 10
+      sum += digit ** 3
+      temp //= 10
+   if num == sum:
+      print(num, "is an Armstrong number")
+   else:
+      print(num, "is not an Armstrong number")
 
-# initialize sum
-sum = 0
-
-# find the sum of the cube of each digit
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
-
-# display the result
-if num == sum:
-   print(num,"is an Armstrong number")
-else:
-   print(num,"is not an Armstrong number")
+# Input & function call
+num = int(input("Please enter the number to be checked: "))
+checkArmstrong(num)
